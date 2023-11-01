2cb376137e989f3788644e8b7e0a31e3cd9bebaf
luuk0406 <luukf0406@gmail.com>
2023-11-01 16:12:52 +0100

    change index

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { MortgageCalculator } = require("./MortgageCalculator");
 const person1 = new Person(30000, false);
 const person2 = new Person(30000, false);
 
-const mortgageCalculator = new MortgageCalculator(30, 9681, [person1, person2]);
+const mortgageCalculator = new MortgageCalculator(30, 1234, [person1, person2]);
 
 console.log("Total mortgage:", mortgageCalculator.calculateMaxMortgage());
 console.log(
